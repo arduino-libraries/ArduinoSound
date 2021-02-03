863b8a4d5e196585b139feb765cde36aaf41f469
Tomas Pilny <tomas.pilny@espressif.com>
2021-02-03 08:40:03 +0100

    [MAINTENANCE] Updated EmbededPlay

--- examples/ESP_CPP/EmbededPlay/main/EmbededPlay.cpp
+++ examples/ESP_CPP/EmbededPlay/main/EmbededPlay.cpp
@@ -2,22 +2,35 @@
  Example for Espressif generic ESP32
  In this example we embed short WAV sound file into code.
  This enables you to play short sound without any external storage.
- To keep things minimal we are using integrated Digital-to-Analog Converter (DAC).
- To propperly drive the speaker you will need any audio amplifier.
+ However you will need aditional module to drive speakers or headphones
+
+ Hardware:
+   - Any ESP device
+   - Option 1 - audio amplifier
+     - please keep attantion to intended impedance and wattege of speakers
+       * Speaker wattege must be less or equal to wattage of amplifier
+       * Impedance of speakers must match exactly impedance of amplifier
+   - Option 2 - I2S codec chip module / I2S decoder board
+   - Headphones or speares according to chosen amplifier
 
  Setup:
-   Connect your audio amplifier to ESP.
-     ESP32 has DAC on GPIO pins 25 and 26.
-     ESP32-S2 has DAC on GPIO pins 17 and 18.
-   Connect speaker to amplifier.
+   Connect your audio amplifier or I2S decoder to ESP.
+    - For audio amplifier note:
+      * ESP32 has DAC on GPIO pins 25 and 26.
+      * ESP32-S2 has DAC on GPIO pins 17 and 18.
+    - For I2S decoder
+      * Connect CLK, WS and DIN to any 3 GPIO pins and modify code below line with USE_I2S_MODULE
+      * Remaining pins on module are specific and you will have to consult datasheet
+      * Uncomment line #define USE_I2S_MODULE
+   Connect speaker(s) or headpghones.
    Connect ESP to computer with USB and load.
    If you want to trigger the playback with button press:
-     Connect your button to any GPIO (in this sketch 12) and GND
-     Uncommet line #define USE_BUTTON located below this comment block
+    - Connect your button to any GPIO (in this sketch 12) and GND
+    - Uncomment line #define USE_BUTTON located below this comment block
    If you want to load your own sound file use name "sound_file.wav"
-     and copy it over default file in folder main.
-     If you want to port this functionality you should read more here
-     https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#embedding-binary-data
+    - and copy it over default file in folder main.
+    - If you want to port this functionality you should read more here
+    - https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#embedding-binary-data
 
  Flashing:
    If upload doesnt start automatically press and hold Boot button and shortly press RST button.
@@ -34,7 +47,17 @@
 #include <ArduinoSound.h>
 
 //#define USE_BUTTON
-const int button_pin = GPIO_NUM_12;
+#ifdef USE_BUTTON
+  const int button_pin = GPIO_NUM_12;
+#endif
+
+//#define USE_I2S_MODULE // uncomment this line to use external I2S module instead of default amplfier with integrated DAC
+#ifdef USE_I2S_MODULE
+  int bit_clock_pin = 32; // Main clock for the external I2S codec (sometimes called SCK or BCK)
+  int word_select_pin = 33; // Signal for changind Left and Right channel (sometimes called WS or LCK)
+  int data_out_pin = 25; // Main signal with I2s data (sometimes called DIN, or SD)
+#endif
+
 
 // symbol names of file's start and end
 extern const uint8_t sound_file_wav_start[] asm("_binary_sound_file_wav_start");
@@ -79,8 +102,6 @@ bool play_embedded_wav_file(const uint8_t *file_start, const uint8_t *file_end){
   WaveFileHeader header;
   uint32_t data_offset;
   Serial.println("play_embedded_wav_file()");
-  Serial.print("file_start = "); Serial.println((int)file_start);
-  Serial.print("file_end = "); Serial.println((int)file_end);
   Serial.print("file size = "); Serial.println((int)file_end - (int)file_start);
   bool file_ok = read_embeded_wav_header(file_start, file_end, &header, &data_offset);
 
@@ -106,7 +127,11 @@ bool play_embedded_wav_file(const uint8_t *file_start, const uint8_t *file_end){
     bitsPerSample = 16;
     sampleRate = sampleRate /2;
   }
+#ifndef USE_I2S_MODULE
   AudioOutI2S.beginDAC(sampleRate);
+#else
+  AudioOutI2S.outBegin(sampleRate, bitsPerSample, bit_clock_pin, word_select_pin, data_out_pin);
+#endif
 
   // Adjust the playback volume
   Serial.println("set volume...");
